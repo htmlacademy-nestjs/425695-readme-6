d2d4909da66b17e0115023cb71231292c3dfa9c8
Kotov Dmitriy <kmg.dev@mail.ru>
2024-04-04 22:28:15 +0300

    2.15. внедряет в качестве зависимости `UserModule` в `AuthenticationModule`

--- project/libs/account/authentication/src/authentication.module.ts
+++ project/libs/account/authentication/src/authentication.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { UserModule, UserRepository } from '@project/user'
 
 @Module({
+  imports: [UserModule],
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
 })
-export class AuthenticationModule {}
+export class AuthenticationModule {
+  constructor(
+    private readonly userRepository: UserRepository
+  ) {}
+}
