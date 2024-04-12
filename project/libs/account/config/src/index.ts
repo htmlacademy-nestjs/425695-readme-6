8321bef5322971fe08fd76303abc1d5f0b7fd6ef
Kotov Dmitriy <kmg.dev@mail.ru>
2024-04-12 20:08:05 +0300

    3.17. подключает `MongooseModule` в корневой модуль сервиса `account`

--- project/libs/account/config/src/index.ts
+++ project/libs/account/config/src/index.ts
@@ -2,3 +2,5 @@ export {AccountConfigModule} from './lib/account-config';
 
 export { default as applicationConfig } from './configurations/app.config';
 export { default as dbConfig } from './configurations/mongo.config';
+
+export { getMongooseOptions } from './configurations/mongodb/get-mongoose-options';
