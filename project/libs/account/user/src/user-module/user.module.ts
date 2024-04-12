95888ab851a47eb5d8a0112fcebceb63065d65da
Kotov Dmitriy <kmg.dev@mail.ru>
2024-04-12 20:21:44 +0300

    3.20. подключает `UserModel` в `UserModule`

--- project/libs/account/user/src/user-module/user.module.ts
+++ project/libs/account/user/src/user-module/user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'
 import { UserFactory } from './user.factory';
 import { UserRepository } from './user.repository';
+import {UserModel,UserSchema} from './user.model';
 
 @Module({
+  imports: [MongooseModule.forFeature([
+    { name: UserModel.name, schema: UserSchema }
+  ])],
   providers: [UserFactory, UserRepository],
   exports: [UserRepository],
 })
