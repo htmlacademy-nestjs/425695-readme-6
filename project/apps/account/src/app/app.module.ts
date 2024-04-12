35d8c54a7b17e92dde698881fd5babfcf48c6b29
Kotov Dmitriy <kmg.dev@mail.ru>
2024-04-12 18:01:34 +0300

    3.8. подключает `AccountConfigModule` в `AppModule`

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '@project/user';
 import { AuthenticationModule } from '@project/authentication'
+import { AccountConfigModule } from '@project/account-config'
 
 @Module({
   imports: [
     UserModule,
     AuthenticationModule,
+    AccountConfigModule,
   ],
   controllers: [],
   providers: [],
