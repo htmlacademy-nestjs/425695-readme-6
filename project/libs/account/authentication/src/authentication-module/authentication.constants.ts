9366d73ab1c5fc5bd1841a72df0f2e65a491aa2a
Kotov Dmitriy <kmg.dev@mail.ru>
2024-04-12 17:27:00 +0300

    3.1. добавляет аннотации для OpenAPI

--- project/libs/account/authentication/src/authentication-module/authentication.constants.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.constants.ts
@@ -3,3 +3,12 @@ export const AuthUser = {
   NOT_FOUND: 'User not found',
   PASSWORD_WRONG: 'User password is wrong'
 } as const;
+
+export const AuthenticationResponseMessage = {
+  LOGGED_SUCCESS: 'User has been successfully logged.',
+  LOGGED_ERROR: 'Password or Login is wrong.',
+  USER_FOUND: 'User found',
+  USER_NOT_FOUND: 'User not found',
+  USER_EXISTS: 'User with the email already exists',
+  USER_CREATED: 'The new user has been successfully created.',
+} as const;
--- project/libs/account/authentication/src/rdo/user.rdo.ts
+++ project/libs/account/authentication/src/rdo/user.rdo.ts
@@ -1,15 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class UserRdo {
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: '134ce8babd-cc30-4805-9b12-d9420398e7c5',
+  })
   @Expose()
   public id: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.local'
+  })
   @Expose()
   public email: string;
 
+  @ApiProperty({
+    description: 'User first name',
+    example: 'firstName',
+  })
   @Expose()
   public firstName: string;
 
+  @ApiProperty({
+    description: 'User last name',
+    example: 'lastName'
+  })
   @Expose()
   public lastName: string;
 }
