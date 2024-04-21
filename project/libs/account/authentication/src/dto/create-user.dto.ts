0251259b1d3c648932c3da0c0143d31c9fd031d8
Kotov Dmitriy <kmg.dev@mail.ru>
2024-04-21 15:31:53 +0300

    5.1. добавляет валидацию для сервиса `account`

--- project/libs/account/authentication/src/dto/create-user.dto.ts
+++ project/libs/account/authentication/src/dto/create-user.dto.ts
@@ -1,27 +1,33 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsString } from 'class-validator';
 
+import { AuthenticationValidateMessage } from '../authentication-module/authentication.constants';
 export class CreateUserDto {
   @ApiProperty({
     description: 'User unique address',
     example: 'user@user.ru'
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EMAIL_NOT_VALID })
   public email: string;
 
   @ApiProperty({
     description: 'User first name',
     example: 'firstName',
   })
+  @IsString()
   public firstName: string;
 
   @ApiProperty({
     description: 'User last name',
     example: 'lastName'
   })
+  @IsString()
   public lastName: string;
 
   @ApiProperty({
     description: 'User password',
     example: 'password'
   })
+  @IsString()
   public password: string;
 }
