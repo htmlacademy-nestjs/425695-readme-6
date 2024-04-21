c1fb38f0efdf5c469f011667323cc8bf7595319e
Kotov Dmitriy <kmg.dev@mail.ru>
2024-04-21 15:40:40 +0300

    5.2. подчключает `ValidationPipe` в корень сервиса `account`

--- project/apps/account/src/main.ts
+++ project/apps/account/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe  } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
@@ -24,6 +24,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({transform: true}));
 
   const configService = app.get(ConfigService);
   const port = configService.get('application.port');
