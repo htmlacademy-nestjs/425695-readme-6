7b95118a98d638686d3673d5658083dc9edfdb75
Kotov Dmitriy <kmg.dev@mail.ru>
2024-04-27 16:03:05 +0300

    5.6. подключает `PostModule` и `ValidationPipe`

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { CategoryModule } from '@project/blog-category';
 import { CommentModule } from '@project/blog-comment';
+import { PostModule } from '@project/blog-post';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [CategoryModule, CommentModule],
+  imports: [PostModule, CategoryModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
