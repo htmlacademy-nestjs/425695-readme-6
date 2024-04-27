e8953a042e6fc2ba211c917b810435e9b868c272
Kotov Dmitriy <kmg.dev@mail.ru>
2024-04-27 18:43:59 +0300

    5.12. добавляет настройки для JWT

--- project/libs/account/config/src/index.ts
+++ project/libs/account/config/src/index.ts
@@ -2,5 +2,6 @@ export {AccountConfigModule} from './lib/account-config';
 
 export { default as applicationConfig } from './configurations/app.config';
 export { default as dbConfig } from './configurations/mongo.config';
+export { default as jwtConfig } from './configurations/jwt.config';
 
 export { getMongooseOptions } from './configurations/mongodb/get-mongoose-options';
