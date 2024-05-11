7aac69f546dc18881cf2035cc7a2ec1ffae3a612
Kotov Dmitriy <kmg.dev@mail.ru>
2024-05-11 15:58:00 +0300

    6.23. добавляет модуль `mail` в `notify`

--- project/libs/notify/email-subscriber/src/index.ts
+++ project/libs/notify/email-subscriber/src/index.ts
@@ -1,2 +1,3 @@
 export { EmailSubscriberModule } from './email-subscriber.module';
 export {CreateSubscriberDto} from './dto/create-subscriber.dto'
+export { MailModule } from './mail-module/mail.module';
